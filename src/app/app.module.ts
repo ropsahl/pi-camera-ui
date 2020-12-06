a85b35525510289f5057f42f387f1945d690ede6
Runar Opsahl <runar.opsahl@telenor.com>
2020-12-06 14:23:01 +0100

    ng add @alyle/ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
-import {BrowserModule} from '@angular/platform-browser';
+import {BrowserModule, HammerModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import {AppComponent} from './app.component';
 import {ConfigEditorComponent} from './config-editor/config-editor.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { LyThemeModule, LY_THEME, LY_THEME_NAME, StyleRenderer, LyTheme2 } from '@alyle/ui';
+import { MinimaLight } from '@alyle/ui/themes/minima';
 
 @NgModule({
   declarations: [
@@ -15,8 +17,9 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+    HammerModule,
   ],
-  providers: [],
+  providers: [StyleRenderer, LyTheme2, { provide: LY_THEME_NAME, useValue: 'minima-light' }, { provide: LY_THEME, useClass: MinimaLight, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule {
